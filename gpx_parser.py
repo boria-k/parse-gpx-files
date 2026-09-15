25756d867cce5d920532291f443f251a1822e1ea
boria-k <47387777+boria-k@users.noreply.github.com>
2026-09-15 12:12:21 +0300

    Update gpx_parser.py

--- gpx_parser.py
+++ gpx_parser.py
@@ -83,11 +83,11 @@ if uploaded_file:
     a, b = st.columns(2)
     c, d = st.columns(2)
 
-    a.metric(label='Number of points', value = len(df), border=True)
-    b.metric(label = 'Total Ascend (metres)', value = int(totalAscend), border=True)
-    c.metric(label='Max height diff (metres)', value=int(heightDiff), border=True)
-    d.metric(label='Distance (metres)',value=int(totalDist), border=True)
-    st.metric(label='Time', value=df.iloc[-1]['time'], border=True)
+    a.metric(label='Time', value=df.iloc[-1]['time'], border=True)
+    b.metric(label='Distance (metres)',value=int(totalDist), border=True)
+    
+    c.metric(label = 'Total Ascend (metres)', value = int(totalAscend), border=True)
+    d.metric(label='Max height diff (metres)', value=int(heightDiff), border=True)
 
 
     fig = px.area(df[::5], x = 'distanceAcc', y = 'elevation',range_y=[df['elevation'].min()-10, df['elevation'].max() + 10], line_shape='spline')
